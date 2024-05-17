331dc85f8a4b982b618f11507c7e39a83c660f54
amethyst0313 <amethystubay23@gmail.com>
2024-05-17 18:51:37 +0800

    adding initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+let array_of_numbers = [1, 3, 9, 15, 90];
+let avg = averageArray(array_of_numbers);
+
+function averageArray(arr) {
+  let n = arr.length;
+  let sum = 0;
+  let average;
+
+  for (let i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+
+  average = sum / n;
+
+  return average;
+}
+
+console.log("Average: ", avg);
